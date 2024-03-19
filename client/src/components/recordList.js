285cb0b0c32043dd011dc3fbf09d5872b7d62caa
MaximeMalpica <m.malpicaarana@students.ephec.be>
2024-03-19 15:08:54 +0100

    Update acceuil

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -3,18 +3,9 @@ import { Link } from "react-router-dom";
 const Record = (props) => (
  <tr>
    <td>{props.record.name}</td>
-   <td>{props.record.position}</td>
-   <td>{props.record.level}</td>
-   <td>
-     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
-     <button className="btn btn-link"
-       onClick={() => {
-         props.deleteRecord(props.record._id);
-       }}
-     >
-       Delete
-     </button>
-   </td>
+   <td>{props.record.Bio}</td>
+   <td>{props.record.address}</td>
+   <td>{props.record.price}</td>
  </tr>
 );
 export default function RecordList() {
@@ -57,14 +48,14 @@ export default function RecordList() {
   // This following section will display the table with the records of individuals.
  return (
    <div>
-     <h3>Record List</h3>
+     <h3>Liste de tatoueurs</h3>
      <table className="table table-striped" style={{ marginTop: 20 }}>
        <thead>
          <tr>
-           <th>Name</th>
-           <th>Position</th>
-           <th>Level</th>
-           <th>Action</th>
+           <th>Nom</th>
+           <th>Bio</th>
+           <th>Lieu</th>
+           <th>Prix</th>
          </tr>
        </thead>
        <tbody>{recordList()}</tbody>
