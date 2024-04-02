58e9467a5a084c75fac0749f07b2bc75b428b9cd
Chicagolil <he202305@students.ephec.be>
2024-04-02 14:40:02 +0200

    Update carte.js

--- client/src/components/carte.js
+++ client/src/components/carte.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import customIcon from 'D:/LILIAN/EPHEC/2ième/Dev3/TATOO/tattooReactJS/client/src/images/points.png'; // Chemin vers votre icône personnalisée
+import customIcon from '../images/points.png'; // Chemin vers votre icône personnalisée
 
 export default function MapWithMarkers() {
   const [records, setRecords] = useState([]);
