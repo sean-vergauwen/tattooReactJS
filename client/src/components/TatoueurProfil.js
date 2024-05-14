2d0c90161668a80291ea260e6a656af6a36151e7
MaximeMalpica <m.malpicaarana@students.ephec.be>
2024-05-14 18:30:05 +0530

    display all like tattoo in one page

--- client/src/components/TatoueurProfil.js
+++ client/src/components/TatoueurProfil.js
@@ -1,106 +1,179 @@
-import { useParams } from 'react-router-dom';
-import styles2 from './content.module.css'
-import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import styles2 from "./content.module.css";
+import { useEffect, useState } from "react";
 
 function TatoueurProfil() {
-    let { id } = useParams();
+  let { id } = useParams();
 
-    const [tatoueur, setTatoueur] = useState(null);
-    const [showReviewInput, setShowReviewInput] = useState(false); // Pour afficher/cacher l'input
-    const [reviewText, setReviewText] = useState(''); // Pour stocker le texte de l'avis
+  const [tatoueur, setTatoueur] = useState(null);
+  const [showReviewInput, setShowReviewInput] = useState(false);
+  const [userData, setUserData] = useState();
 
+  const [reviewText, setReviewText] = useState("");
 
-    useEffect(() => {
-        async function getRecords() {
-            const response = await fetch(`http://localhost:5001/record/`);
-            if (!response.ok) {
-                const message = `Une erreur est survenue : ${response.statusText}`;
-                window.alert(message);
-                return;
+  const handleAllData = () => {
+    const data = localStorage.getItem("userData");
+    const storageData = JSON.parse(data);
+    setUserData(storageData);
+    if (storageData) {
+      try {
+        fetch(`http://localhost:3000/user/tattoo/${id}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${storageData?.data?.token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            if (responseData.statusCode === 200) {
+              setTatoueur(responseData?.data);
+            } else {
+              window.alert(responseData?.message);
             }
-            const records = await response.json();
-            // Trouver le tatoueur après la mise à jour des records
-            const lebon = records.find(tatoueur => tatoueur["_id"] === id);
-            setTatoueur(lebon);
-        }
-        getRecords();
-    }, [id])
-
-    const handleSaveReview = async () => {
-        try {
-            const response = await fetch(`http://localhost:5001/avis/${id}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ avis: reviewText }), // Envoie l'avis comme partie du corps de la requête
-            });
-
-            if (!response.ok) {
-
-                throw new Error(`Une erreur est survenue : ${response.statusText}`);
-
-            }
-
-
-            console.log("Avis enregistré :");
-
-
-        } catch (error) {
-            console.error("Erreur lors de l'enregistrement de l'avis:", error);
-            window.alert("Erreur lors de l'enregistrement de l'avis.");
-        } finally {
-            // Réinitialise le formulaire d'avis indépendamment du résultat de la requête
-            setShowReviewInput(false);
-            setReviewText('');
-        }
-    };
-
-    if (!tatoueur) {
-        // Gérer l'affichage pendant le chargement ou si le tatoueur n'est pas trouvé
-        return <div>Chargement des détails du tatoueur...</div>;
+          })
+          .catch((error) => {
+            console.error(error);
+            window.alert(error);
+          });
+      } catch (error) {
+        console.error(error);
+        window.alert(error);
+      }
     }
-    return (
-        <div className="flex-fill container p-20">
-            <h1 className='my-30'>Profil du Salon  {tatoueur.name} </h1>
-            <div className={`d-flex flex-column card p-20 ${styles2.contentCard} `}>
-                <p>Nom : {tatoueur.name} </p>
-                <p>Addresse : {tatoueur.address} </p>
-                <p>Identifiant : {id} </p>
-                <p>Site web : <a href={tatoueur.website} target='_blank'>{tatoueur.website}</a> </p>
-                <button onClick={() => setShowReviewInput(true)} className='btn btn-primary d-flex  p-10 mb-20' >Ajouter en favoris</button>
-                <h3 className='my-30'>Avis : </h3>
-                {showReviewInput ? (
-                    <>
-                        <textarea
-                            className='mb-20 textarea-avis'
-                            value={reviewText}
-                            onChange={(e) => setReviewText(e.target.value)}
-                            placeholder="Laissez votre avis ici..."
-                        />
-                    
-                        <button onClick={handleSaveReview} className='btn btn-primary d-flex   p-10 mb-20'>Enregistrer l'avis</button>
-                    </>
-                ) : (
-                
+  };
 
-                    <button onClick={() => setShowReviewInput(true)} className='btn btn-primary d-flex  p-10 mb-20' >Laisser un avis</button>
-                )}
-                <div className={`d-flex flex-column card p-20 ${styles2.contentCard} `}>
-                    {tatoueur.avis && tatoueur.avis.length > 0 ? (
-                        tatoueur.avis.map((avis, index) => (
-                            <p className={`d-flex flex-column card p-20 ${styles2.contentCard} `} key={index}>Avis de {avis._id} {index + 1}: {avis.avis}</p>
-                        ))
-                    ) : (
-                        <p>Aucun avis pour le moment.</p>
-                    )}
-                </div>
+  useEffect(() => {
+    const data = localStorage.getItem("userData");
+    const storageData = JSON.parse(data);
+    setUserData(storageData);
+    if (storageData) {
+      handleAllData();
+    }
+  }, [id]);
 
-            </div>
+  const handleSaveReview = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/user/comment`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userData?.data?.token}`,
+        },
+        body: JSON.stringify({ id: id, comment: reviewText }),
+      });
 
+      if (response.status === 200) {
+        handleAllData();
+      } else {
+        throw new Error(`Une erreur est survenue : ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'enregistrement de l'avis:", error);
+      window.alert("Erreur lors de l'enregistrement de l'avis.");
+    } finally {
+      // Réinitialise le formulaire d'avis indépendamment du résultat de la requête
+      setShowReviewInput(false);
+      setReviewText("");
+    }
+  };
+
+  const handleAddFavourite = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/user/like/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userData?.data?.token}`,
+        },
+      });
+      const responseData = await response.json();
+      if (responseData.statusCode === 200) {
+        window.alert(responseData?.message);
+      } else {
+        throw new Error(`Une erreur est survenue : ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'enregistrement de l'avis:", error);
+      window.alert("Erreur lors de l'enregistrement de l'avis.");
+    } finally {
+      // Réinitialise le formulaire d'avis indépendamment du résultat de la requête
+      setShowReviewInput(false);
+      setReviewText("");
+    }
+  };
+
+  if (!tatoueur) {
+    // Gérer l'affichage pendant le chargement ou si le tatoueur n'est pas trouvé
+    return <div>Chargement des détails du tatoueur...</div>;
+  }
+
+  console.log("reviewText", tatoueur);
+
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Profil du Salon {tatoueur.name} </h1>
+      <div className={`d-flex flex-column card p-20 ${styles2.contentCard} `}>
+        <p>Nom : {tatoueur.name} </p>
+        <p>Addresse : {tatoueur.description} </p>
+        <p>Identifiant : {id} </p>
+        <p>
+          Site web :{" "}
+          <a href={tatoueur.website} target="_blank">
+            {tatoueur.website}
+          </a>{" "}
+        </p>
+        <button
+          onClick={() => handleAddFavourite(tatoueur?._id)}
+          className="btn btn-primary d-flex  p-10 mb-20"
+        >
+          Ajouter en favoris
+        </button>
+        <h3 className="my-30">Avis : </h3>
+        {showReviewInput ? (
+          <>
+            <textarea
+              className="mb-20 textarea-avis"
+              value={reviewText}
+              onChange={(e) => setReviewText(e.target.value)}
+              placeholder="Laissez votre avis ici..."
+            />
+            <button
+              className="btn btn-primary d-flex   p-10 mb-20"
+              onClick={handleSaveReview}
+              disabled={reviewText.length < 1}
+            >
+              Enregistrer l'avis
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => setShowReviewInput(true)}
+            className="btn btn-primary d-flex  p-10 mb-20"
+          >
+            Laisser un avis
+          </button>
+        )}
+        <div className={`d-flex flex-column card p-20 ${styles2.contentCard} `}>
+          {tatoueur.comments && tatoueur.comments.length > 0 ? (
+            tatoueur.comments.map((avis, index) => {
+              console.log("avis", avis);
+              return (
+                <p
+                  className={`d-flex flex-column card p-20 ${styles2.contentCard} `}
+                  key={index}
+                >
+                  Avis de {avis.userName ? avis.userName : "------- "}{" "}
+                  {index + 1}: {avis.comment}
+                </p>
+              );
+            })
+          ) : (
+            <p>Aucun avis pour le moment.</p>
+          )}
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
 
 export default TatoueurProfil;
---
+++ client/src/components/favoris.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import styles from "./content.module.css";
+import Tatoueurs from "./Tatoueurs.js";
+import { data } from "../data/tattoo.js";
+
+export default function Favoris({}) {
+  const [records, setRecords] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [tattooData, setTattooData] = useState([]);
+  const [userData, setUserData] = useState();
+
+  const handleAllData = () => {
+    const data = localStorage.getItem("userData");
+    const storageData = JSON.parse(data);
+    setUserData(storageData);
+    if (storageData) {
+      try {
+        fetch("http://localhost:3000/user/all-likes", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${storageData?.data?.token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((responseData) => {
+            if (responseData.statusCode === 200) {
+              setTattooData(responseData?.data?.tattoos);
+            } else {
+              window.alert(responseData?.message);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            window.alert(error);
+          });
+      } catch (error) {
+        console.error(error);
+        window.alert(error);
+      }
+    }
+  };
+
+  function handleInput(e) {
+    const filter = e.target.value.trim().toLowerCase();
+    const filteredData = tattooData.filter((tattoo) =>
+      tattoo.name.toLowerCase().includes(filter)
+    );
+    setFilter(filter);
+    if (filter.length >= 1) {
+      setTattooData(filteredData);
+    } else {
+      handleAllData();
+    }
+  }
+
+  useEffect(() => {
+    const data = localStorage.getItem("userData");
+    const storageData = JSON.parse(data);
+    setUserData(storageData);
+    if (storageData) {
+      handleAllData();
+    }
+  }, []);
+
+  // Delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:3000/${id}`, {
+      method: "DELETE",
+    });
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  // Display the table with the records of individuals
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos tatoueurs </h1>
+      <div className={`d-flex flex-column card p-20 ${styles.contentCard} `}>
+        <div
+          className={`my-30 d-flex flex-row justify-content-center allign-items-center ${styles.searchBar}`}
+        >
+          <input
+            onInput={handleInput}
+            className="flex-fill"
+            type="text"
+            placeholder="Rechercher"
+          />
+        </div>
+        {tattooData?.length > 0 || tattooData?.length != null ? (
+          <div className={styles.grid}>
+            {tattooData?.map((r) => (
+              <Tatoueurs
+                id={r._id}
+                name={r.name}
+                photoDeProfil={r.image}
+                address={r.description}
+                Favoris={true}
+              />
+            ))}
+          </div>
+        ) : (
+          "Aucune donnée disponible"
+        )}
+      </div>
+    </div>
+  );
+}
